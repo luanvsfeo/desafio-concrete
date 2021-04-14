975f84f75c6e00593e182ff82dcf1507d78d9321
Luan Feo <luanfeo@gmail.com>
2021-04-14 11:06:54 -0300

    classes faltantes

---
+++ src/main/java/com/luan/desafioconcrete/DesafioconcreteApplication.java
@@ -0,0 +1,13 @@
+package com.luan.desafioconcrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioconcreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioconcreteApplication.class, args);
+	}
+
+}
